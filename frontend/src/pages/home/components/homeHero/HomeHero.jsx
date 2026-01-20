57b66eaabd8866203ec46038bfb9aedce1385432
Kalpesh Parab <abhi9405.at@gmail.com>
2026-01-21 01:38:04 +0530

    feat(home): refine hero component and styles

--- frontend/src/pages/home/components/homeHero/HomeHero.jsx
+++ frontend/src/pages/home/components/homeHero/HomeHero.jsx
@@ -1,11 +1,25 @@
-import "./homeHero.scss"
+import './homeHero.scss';
+import bg from '../../../../assets/homeHero.mp4';
+import arrow from '../../../../assets/arrow.svg';
 
 const HomeHero = () => {
   return (
-      <div className="HomeHero">
-          
-    </div>
-  )
-}
+    <div className='HomeHero'>
+      <video src={bg}></video>
+      <div className='title'>
+        Transforming Dreams into Realities One Happy Traveler at a Time!
+      </div>
+      <div className='subTitle'>
+        Our passion for exploration fuels us to craft personalized experiences
+        that dive deep into the heart of each destination. Let’s make your
+        travel dreams a reality!
+      </div>
 
-export default HomeHero
+          <div className='button'>Explore Destinations
+              <img src={arrow} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomeHero;
