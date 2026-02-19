0e95e5ee3d7bb52eeebab7bfad7514430e54c99d
Kalpesh Parab <abhi9405.at@gmail.com>
2026-02-19 14:55:24 +0530

    feat - added destSelector with Indian map and world map

--- frontend/src/pages/destination/Destination.jsx
+++ frontend/src/pages/destination/Destination.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DestHero from './sections/destHero/DestHero';
+import DestSelector from './sections/destSelector/DestSelector';
 
 const Destination = () => {
   return (
     <>
       <DestHero />
+      <DestSelector />
     </>
   );
 };
---
+++ frontend/src/pages/destination/sections/destSelector/DestSelector.jsx
@@ -0,0 +1,144 @@
+import './destSelector.scss';
+import i1 from '../../../../assets/destination/plan.svg';
+import i2 from '../../../../assets/destination/globesearch.svg';
+import i3 from '../../../../assets/destination/likeperson.svg';
+// import mh from '../../../../assets/v1.MP4';
+import mh from '../../../../assets/b1.png';
+import gj from '../../../../assets/v2.MP4';
+import { useEffect, useRef, useState } from 'react';
+import IndiaMap from './IndiaMap';
+import WorldMap from './WorldMap';
+
+const DestSelector = () => {
+  const [tooltip, setTooltip] = useState({
+    visible: false,
+    x: 0,
+    y: 0,
+    text: '',
+  });
+
+  //   const [activeState, setActiveState] = useState(null);
+  //   const svgRef = useRef(null);
+  //   const [bbox, setBbox] = useState(null);
+  const [mapType, setMapType] = useState('domestic');
+
+  //   const handleHover = (id) => {
+  //     if (!svgRef.current) return;
+
+  //     const el = svgRef.current.querySelector(`#${id}`);
+  //     if (!el) return;
+
+  //     const box = el.getBBox();
+
+  //     setActiveState(id);
+  //     setBbox(box);
+  //   };
+
+  useEffect(() => {
+    // Reset hover when switching maps
+  }, [mapType]);
+
+  //   const handleLeave = () => {
+  //     setActiveState(null);
+  //     setBbox(null);
+  //   };
+
+  const tabs = [
+    {
+      icon: i1,
+      name: `Bespoke Planning`,
+    },
+    {
+      icon: i2,
+      name: `Handpicked Destinations`,
+    },
+    {
+      icon: i3,
+      name: `Seamless Experiences`,
+    },
+  ];
+
+  const stateMedia = {
+    'IN-MH': {
+      name: 'Maharashtra',
+      media: mh,
+      type: 'image',
+    },
+    'IN-GJ': {
+      name: 'Gujarat',
+      media: gj,
+      type: 'video',
+    },
+  };
+  const countryMedia = {
+    IN: {
+      name: 'India',
+      media: gj,
+      type: 'video',
+    },
+  };
+  return (
+    <section className='DestSelector'>
+      <div className='top'>
+        <h4>Destination</h4>
+        <div className='buttons'>
+          <div
+            className={`button ${mapType === 'domestic' ? 'active' : ''}`}
+            onClick={() => setMapType('domestic')}
+          >
+            Domestic
+          </div>
+
+          <div
+            className={`button ${mapType === 'international' ? 'active' : ''}`}
+            onClick={() => setMapType('international')}
+          >
+            International
+          </div>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='left'>
+          <h2>
+            Let NPGO open the doors to extraordinary destinations across India
+            and beyond, creating journeys that go far beyond travel.
+          </h2>
+          <h4>
+            Whether it's leisure, adventure, romance, or corporate travel, our
+            bespoke journeys are crafted to reflect your style and pace.
+          </h4>
+
+          <div className='tabs'>
+            {tabs.map((tab) => (
+              <div className='tab'>
+                <img src={tab.icon} alt='' />
+                <span>{tab.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='right'>
+          {mapType === 'domestic' ? (
+            <IndiaMap stateMedia={stateMedia} setTooltip={setTooltip} />
+          ) : (
+            <WorldMap countryMedia={countryMedia} setTooltip={setTooltip} />
+          )}
+          {tooltip.visible && (
+            <div
+              className='map-tooltip'
+              style={{
+                left: tooltip.x - 10,
+                top: tooltip.y + 30,
+                scale: 0.8,
+              }}
+            >
+              {tooltip.text}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DestSelector;
