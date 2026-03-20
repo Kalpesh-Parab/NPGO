7afa10c3fc8509470af3769dd33ccb538455b645
Kalpesh Parab <abhi9405.at@gmail.com>
2026-03-21 01:07:18 +0530

    feat: added new apis

--- backend/routes/packageRoutes.js
+++ backend/routes/packageRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 import {
   createPackage,
@@ -7,15 +7,15 @@ import {
   updatePackage,
   deletePackage,
   getPackagesByLocation,
-} from "../controllers/packageController.js";
+} from '../controllers/packageController.js';
 
 const router = express.Router();
 
-router.post("/", createPackage);
-router.get("/", getAllPackages);
-router.get("/:slug", getPackageBySlug);
-router.put("/:id", updatePackage);
-router.delete("/:id", deletePackage);
+router.post('/', createPackage);
+router.get('/', getAllPackages);
 router.get('/by-location', getPackagesByLocation);
+router.get('/:slug', getPackageBySlug);
+router.put('/:id', updatePackage);
+router.delete('/:id', deletePackage);
 
 export default router;
